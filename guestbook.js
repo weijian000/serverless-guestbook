9ad3a7dd4a7946c1a1c80a12e4551a112a891d83
weijian000 <92417992+weijian000@users.noreply.github.com>
2021-12-24 17:18:27 +0800

    update

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://a26ac33d.au-syd.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
